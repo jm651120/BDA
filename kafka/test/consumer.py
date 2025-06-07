8507e381b9f61e8a8379baceffa71290231635f9
Gustavo Gomes <gus_gomes02@hotmail.com>
2025-06-07 18:03:33 +0100

    Delete kafka directory

--- kafka/test/consumer.py
+++
@@ -1,7 +0,0 @@
-import json
-from kafka import KafkaConsumer
-c = KafkaConsumer("streaming", bootstrap_servers="kafkainterface.servebeer.com:9092",
-                  value_deserializer=lambda v: json.loads(v.decode()))
-for msg in c:
-    print(msg.key, msg.value)
-
